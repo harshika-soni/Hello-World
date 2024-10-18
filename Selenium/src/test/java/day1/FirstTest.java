f104a9c542c68e465364e047ecf4607170f896a3
harshika-soni <harshikamuz@gmail.com>
2024-10-18 02:03:47 -0700

    6

---
+++ Selenium/src/test/java/day1/FirstTest.java
@@ -0,0 +1,16 @@
+package day1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest     {
+	@Test
+	public void m1() {
+		
+		WebDriver driver=new ChromeDriver();
+		System.out.println("i m m1");
+
+	}
+
+}
